0752c1c1ded607000734cd2bb18427f737c3e568
Do Nhat <dmnhat.pt@gmail.com>
2024-03-07 14:15:20 +0700

    first commit

---
+++ frontend/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Amazon Clone',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello.'),
+        ),
+        body: const Center(
+          child: Text('Flutter Demo Home Page'),
+        ),
+      ),
+    );
+  }
+}
