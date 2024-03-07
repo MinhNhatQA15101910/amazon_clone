10ba6f1b57278fd4defc96acfb28e5e3629ed0a3
Do Nhat <dmnhat.pt@gmail.com>
2024-03-07 14:30:53 +0700

    Folder Structure.

---
+++ frontend/lib/features/auth/screens/auth_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() {
+    return _AuthScreenState();
+  }
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
